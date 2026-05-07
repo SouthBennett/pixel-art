16db7fb911130f5131e7ff28a5d5cafcd5a6faec
SouthBennett <146032836+SouthBennett@users.noreply.github.com>
2026-05-06 18:22:52 -0700

    imported useSate and added navbar active state foundation

--- pixel-art/src/components/Header.jsx
+++ pixel-art/src/components/Header.jsx
@@ -1,14 +1,13 @@
+import { useState } from 'react'
 import '../components/Header.css'
 
 export default function Header() {
-  function colorChange() {
-    let element = document.getElementById("nav-link");
-    element.classList.toggle("dark-mode")
-  }
+  const [activeTab, setActiveTab] = useState("Home");
+  
   return (
     <header className="header" id="header">
       <nav>
-        <a onClick={colorChange} href="" className="nav-link">Home</a>
+        <a href="" className="nav-link">Home</a>
         <a href="" className="nav-link">Pixel Art</a>
         <a href="" className="nav-link">Pixel Art+</a>
         <a href="" className="nav-link">Tier List</a>
