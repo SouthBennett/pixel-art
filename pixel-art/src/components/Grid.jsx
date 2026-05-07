20583102c7a58c3c67e1a4a6837a2f45e2554167
Connor <connorjohnhughes@gmail.com>
2026-05-06 21:21:22 -0700

    Rendered grid component, only changes from white to black right now

--- pixel-art/src/components/Grid.jsx
+++ pixel-art/src/components/Grid.jsx
@@ -1,18 +1,39 @@
 import './Grid.css'
+import { useState } from 'react'
+
 
 export default function Grid(){
-    const cells = [];
+    const rows = Array.from({length: 16})
+    const cols = Array.from({length: 16})
 
-    for(let i =0;i < 256;i++){
-        cells.push(
-            <div className='cell'></div>
-        )
+    const [ pixels, setPixels ] = useState(
+        Array(256).fill("#ffffff")
+    )
+
+    function paintPixel(index){
+        const updatedPixels = [...pixels]
+        updatedPixels[index] = "black"
+        setPixels(updatedPixels)
     }
+   
 
 
     return(
         <section className="grid-container">
-            {cells}
+            {rows.map((pixel, rowIndex) => (
+                <div className='row' key={rowIndex} >
+                    {cols.map((_,colIndex) => {
+                        const pixelIndex = rowIndex * 16 + colIndex
+                        return(
+                            <button className='cell' key={colIndex}
+                            onClick={() => paintPixel(pixelIndex)}
+                            style={{backgroundColor: pixels[pixelIndex]}}
+                            ></button>
+                        )
+                        
+                    })}
+                </div>
+            ))}
         </section>
     )
 
